1e8f332e3351f22553d7907373532f62343dd8b6
danielcrubens <dcr.tecnologi@gmail.com>
2023-04-23 19:13:20 -0300

    estruturando projeto :memo:

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'dark-blue':'#1F2632',
+        'green-neon':'#52FFA8',
+        'card':'#323A49',
+        'cyan-light':'#CEE3E9',
+      }
+    },
+  },
+  plugins: [],
+}
