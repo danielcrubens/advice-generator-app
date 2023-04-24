a083efdcf538282401551db8bca7e096a1c11e4d
danielcrubens <dcr.tecnologi@gmail.com>
2023-04-24 10:26:24 -0300

    no-cache :memo:

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 const fetchAdvice = async()=>{
-  const res = await fetch("https://api.adviceslip.com/advice")
+  const res = await fetch("https://api.adviceslip.com/advice",{
+    cache:'no-cache'
+  })
   const data =await res.json()
   console.log(data)
   document.querySelector('#title').innerHTML=`Advice #${data.slip.id}`
